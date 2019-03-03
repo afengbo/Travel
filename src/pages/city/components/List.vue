68a3103939ea2d6d2925d7aff652186bb408a551
afengbo <zfengfeng2@163.com>
2019-03-03 17:42:47 +0800

    city ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -12,20 +12,15 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">龙川</div>
+          <div class="button-wrapper" v-for="city in hotcities" :key="city.id">
+            <div class="button">{{city.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, key) in cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom" v-for="c in item" :key="c.id">{{c.name}}</div>
         </div>
       </div>
     </div>
@@ -36,6 +31,10 @@
 import BScroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    cities: Object,
+    hotcities: Array
+  },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
   }
